2c31e4121de73db74480908b10d47465ea3c4eec
Abhishek Chaurasiya <abhichek2580@gmail.com>
2024-08-24 11:17:28 +0530

    fix: Eslint error

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import "dotenv/config";
 
+
 const port = 4040
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.listen(port, () => {
     console.log(`Server listening on ${port}`)
 })
+
 
